c899dd6c466c5a71bb4b1d543bd22f585ce2bf22
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2021-10-11 02:40:40 +0530

    COMMIT BY GCC 2021

--- Question2/Question2.cpp
+++ Question2/Question2.cpp
@@ -9,27 +9,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 typedef struct
 {
     int cost;
     int amount;
 } Pair;
 
-int compare( const void *arg1, const void *arg2 )
+int compare(const void *arg1, const void *arg2)
 {
-    int l = ((Pair *) arg1)->cost;
-    int r = ((Pair *) arg2)->cost;
-    return (l-r);
+    int l = ((Pair *)arg1)->cost;
+    int r = ((Pair *)arg2)->cost;
+    return (l - r);
 }
 
- 
-int securitiesBuying(int z,int security_value[], int size)
+int securitiesBuying(int z, int security_value[], int size)
 {
-    int no_of_stocks=0;
+    int no_of_stocks = 0;
 
     Pair pairs[size];
-    
+
     for (int i = 0; i < size; i++)
     {
         Pair pair;
@@ -39,52 +37,55 @@ int securitiesBuying(int z,int security_value[], int size)
     }
 
     qsort(pairs, size, sizeof(Pair), compare);
-    
-    
+
     int sum = 0;
     for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j < pairs[i].amount; j++)
+        int newSum = sum + pairs[i].amount * pairs[i].cost;
+        int diff = z - newSum;
+        if (diff >= 0)
         {
-            sum += pairs[i].cost;
-            if (sum > z){
+            no_of_stocks += pairs[i].amount;
+            if (diff == 0)
+            {
                 return no_of_stocks;
             }
-            no_of_stocks++;
+            sum = newSum;
         }
-        
+        else
+        {
+            int overflow = -diff / pairs[i].cost;
+            int rest = diff % pairs[i].cost;
+            return no_of_stocks + pairs[i].amount - overflow - (rest != 0);
+        }
     }
 
     return no_of_stocks;
 }
- 
-int main(){
- 
-int z;
-cin>>z;
-vector<int> input_data;
-string buffer;
-int data;
-getline(cin, buffer);
-getline(cin, buffer);
-istringstream iss(buffer);
- 
- 
-while (iss >> data)
-    input_data.push_back(data);
- 
-int n= input_data.size();
- 
- 
- int security_value[n];
-    for (int i = 0; i < n; i++) {
+
+int main()
+{
+
+    int z;
+    cin >> z;
+    vector<int> input_data;
+    string buffer;
+    int data;
+    getline(cin, buffer);
+    getline(cin, buffer);
+    istringstream iss(buffer);
+
+    while (iss >> data)
+        input_data.push_back(data);
+
+    int n = input_data.size();
+
+    int security_value[n];
+    for (int i = 0; i < n; i++)
+    {
         security_value[i] = input_data[i];
     }
 
-int no_of_stocks_purchased = securitiesBuying(z,security_value, n);
-cout << no_of_stocks_purchased;
- 
- 
+    int no_of_stocks_purchased = securitiesBuying(z, security_value, n);
+    cout << no_of_stocks_purchased;
 }
- 
- 
