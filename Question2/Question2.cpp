e79fbfe311dcbe9f03815c45befbcc973f7dea75
Telpenarmo <telpenarmo@protonmail.com>
2021-10-10 22:51:50 +0200

    Question 2 in cpp with passing size

--- Question2/Question2.cpp
+++ Question2/Question2.cpp
@@ -8,13 +8,53 @@
 #include <typeinfo>
 #include <bits/stdc++.h>
 using namespace std;
+
+
+typedef struct
+{
+    int cost;
+    int amount;
+} Pair;
+
+int compare( const void *arg1, const void *arg2 )
+{
+    int l = ((Pair *) arg1)->cost;
+    int r = ((Pair *) arg2)->cost;
+    return (l-r);
+}
+
  
-int securitiesBuying(int z,int security_value[])
+int securitiesBuying(int z,int security_value[], int size)
 {
     int no_of_stocks=0;
-   // participants code here
- 
- 
+
+    Pair pairs[size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        Pair pair;
+        pair.amount = i + 1;
+        pair.cost = security_value[i];
+        pairs[i] = pair;
+    }
+
+    qsort(pairs, size, sizeof(Pair), compare);
+    
+    
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < pairs[i].amount; j++)
+        {
+            sum += pairs[i].cost;
+            if (sum > z){
+                return no_of_stocks;
+            }
+            no_of_stocks++;
+        }
+        
+    }
+
     return no_of_stocks;
 }
  
@@ -40,10 +80,8 @@ int n= input_data.size();
     for (int i = 0; i < n; i++) {
         security_value[i] = input_data[i];
     }
- 
- 
- 
-int no_of_stocks_purchased = securitiesBuying(z,security_value);
+
+int no_of_stocks_purchased = securitiesBuying(z,security_value, n);
 cout << no_of_stocks_purchased;
  
  
