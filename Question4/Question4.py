02456e9e6fd3294ed18724d0d25f22d1104f9b21
Telpenarmo <telpenarmo@protonmail.com>
2021-10-11 18:04:56 +0200

    Q4 in python: apply precomputation

--- Question4/Question4.py
+++ Question4/Question4.py
@@ -1,18 +1,23 @@
 def totalPairs(n, values):
     # Participants code will be here
     result = n-1
+    greater = [None] * n
+    for i in range(n):
+        greater[i] = [None] * n
+        for j in range(n):
+            greater[i][j] = values[i] > values[j]
+
     for left in range(n-2):
-        for right in range(left+2,n):
+        for right in range(left+2, n):
             ok = True
-            for x in range(left+1,right):
-                mid = values[x]
-                if mid>values[left] or values[x]>values[right]:
+            for mid in range(left+1, right):
+                if greater[mid][left] or greater[mid][right]:
                     ok = False
                     break
             if ok:
                 result += 1
-    return result            
-            
+    return result
+
 
 if __name__ == "__main__":
     n = int(input())
