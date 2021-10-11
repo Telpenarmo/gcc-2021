d425dcd5997b4e5f56ce8d08a452166388119d7e
Telpenarmo <telpenarmo@protonmail.com>
2021-10-11 15:53:41 +0200

    Q3 rewrite to cpp

--- Question3/Question3.cpp
+++ Question3/Question3.cpp
@@ -1,8 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int portfolio_profit_maximisation(int maxSum, vector<int> a, vector<int> b) {
-    //write your code here
+int portfolio_profit_maximisation(int maxSum, vector<int> a, vector<int> b)
+{
+    vector<int> a_prefixes = {0};
+    vector<int> b_prefixes = {0};
+    int sum = 0;
+    int a_size = a.size();
+    int a_prefixes_size = 1;
+    for (int i = 0; i < a_size; i++)
+    {
+        sum += a[i];
+        if (sum > maxSum)
+        {
+            break;
+        }
+        a_prefixes.push_back(sum);
+        a_prefixes_size++;
+    }
+    sum = 0;
+    int b_size = b.size();
+    for (int i = 0; i < b_size; i++)
+    {
+        sum += b[i];
+        if (sum > maxSum)
+        {
+            break;
+        }
+        b_prefixes.push_back(sum);
+    }
+
+    int result = 0;
+    int j = 0;
+    int i = 0;
+    j = b_prefixes.size() - 1;
+    for (; i < a_prefixes_size; i++)
+    {
+        while (j >= 0)
+        {
+            int temp = a_prefixes[i] + b_prefixes[j];
+            int pos = i + j;
+            if (temp <= maxSum)
+            {
+                if (pos > result)
+                {
+                    result = pos;
+                }
+                break;
+            }
+            j -= 1;
+        }
+    }
+    return result;
 }
 
 int main()
@@ -15,14 +64,16 @@ int main()
     vector<int> a(n);
     vector<int> b(m);
 
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i)
+    {
         cin >> a[i];
     }
 
-    for (int i = 0; i < m; ++i) {
+    for (int i = 0; i < m; ++i)
+    {
         cin >> b[i];
     }
-    int result = portfolio_profit_maximisation(maxSum, a, b);
+    int result = portfolio_profit_maximisation(x, a, b);
     cout << result;
     return 0;
 }
