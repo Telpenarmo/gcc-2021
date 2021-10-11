c2cd69dbd3d4b915d01fd312f6e08981ebd81f8e
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2021-10-11 23:31:07 +0530

    COMMIT BY GCC 2021

--- Question4/Question4.cpp
+++ Question4/Question4.cpp
@@ -1,28 +1,9 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
+
 #include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
 #include <iostream>
-#include <algorithm>
-#include <unordered_map>
 
 using namespace std;
-int totalPairs(int n, vector<int> &values)
+int totalPairs(int n, vector<int> values)
 {
     int result = 0;
     int max = 0;
