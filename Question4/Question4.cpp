3ac7675f73a300d37bd1c8b514990467833d7474
Telpenarmo <telpenarmo@protonmail.com>
2021-10-11 19:54:30 +0200

    Q4 rewritten to cpp

--- Question4/Question4.cpp
+++ Question4/Question4.cpp
@@ -22,22 +22,45 @@
 #include <unordered_map>
 
 using namespace std;
-int totalPairs(int n, vector<int>& values) {
-    // Complete the total pairs function
-    return -1;
+int totalPairs(int n, vector<int> &values)
+{
+    int result = 0;
+    int max = 0;
+    for (int left = 0; left < n-1; left++)
+    {
+        int end = max == left ? n : max;
+        max = left + 1;
+        result += 1;
+        for (int right = left + 2; right < end; right++)
+        {
+            if (values[right - 1] > values[left])
+            {
+                break;
+            }
+            if (values[right] > values[max])
+            {
+                max = right;
+                result += 1;
+            }
+        }
+    }
+
+    return result;
 }
 
-int main(){
-   int n;
-   cin >> n;
-   vector<int> values(n);
-   for (int i = 0; i < n; ++i) {
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> values(n);
+    for (int i = 0; i < n; ++i)
+    {
         cin >> values[i];
-   }
-	int answer = totalPairs(n, values);
+    }
+    int answer = totalPairs(n, values);
     // Do not remove below line
     cout << answer << endl;
     // Do not print anything after this line
-        
+
     return 0;
 }
