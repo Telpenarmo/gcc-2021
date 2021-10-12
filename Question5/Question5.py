af0d974886564afd027470ecaecedc250aba28a4
Telpenarmo <telpenarmo@protonmail.com>
2021-10-12 14:45:47 +0200

    Q5 python initial concept, not sure if works

--- Question5/Question5.py
+++ Question5/Question5.py
@@ -1,11 +1,35 @@
 
-def sol(n):	
-   #add code
+def sol(n: str):
+    max = 0
+    maxRepeated = False
+    current = 0
+    for c in n:
+        if c == "1":
+            if current > max:
+                max = current
+                maxRepeated = False
+            elif current == max:
+                maxRepeated = True
+            current = 0
+        else:
+            current += 1
+    if current > max:
+        max = current
+        maxRepeated = False
+    elif current == max:
+        maxRepeated = True
+
+    if maxRepeated or max % 2 == 0:
+        return "B"
+    return "A"
 
 # do not edit below code
+
+
 def main():
-	n=input()
-	print(sol(n))
+    n = input()
+    print(sol(n))
+
 
 if __name__ == '__main__':
     main()
